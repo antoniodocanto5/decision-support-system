b6b36bbf6e30be23e95d538955200fd916940d65
antoniodocanto5 <antoniodocanto5@gmail.com>
2024-02-08 12:22:00 -0100

    change some files

--- data_visualization.py
+++ data_visualization.py
@@ -1,2 +1,39 @@
 from main import *
 import matplotlib as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Visualizar a distribuição das características usando histogramas
+netflix_limpo.hist()
+plt.show()
+
+# Verificar colunas com valores não numéricos
+for col in netflix_limpo.columns:
+    if netflix_limpo[col].dtype == 'object':
+        try:
+            pd.to_numeric(netflix_limpo[col])
+        except ValueError:
+            print(f"A coluna {col} contém valores não numéricos.")
+
+# Remover a coluna "Date" do DataFrame
+netflix_limpo_sem_data = netflix_limpo.drop(columns=['Date'])
+
+# Calcular e visualizar a correlação entre as características restantes
+plt.figure(figsize=(8, 6))
+sns.heatmap(netflix_limpo_sem_data.corr(), annot=True, cmap="coolwarm")
+plt.show()
+
+# # Selecionar apenas as colunas numéricas para o agrupamento
+# netflix_numerico = netflix_limpo_sem_data[['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]
+
+# # Aplicar o algoritmo KMeans para realizar o agrupamento
+# kmeans = KMeans(n_clusters=3, random_state=42)
+# kmeans.fit(netflix_numerico)
+
+# # Adicionar rótulos de cluster ao DataFrame
+# netflix_limpo_sem_data['Cluster'] = kmeans.labels_
+
+# # Exibir as primeiras 5 linhas do conjunto de dados com rótulos de cluster
+# print("\nPrimeiras 5 linhas do conjunto de dados com rótulos de cluster:")
+# print(netflix_limpo.head())
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from telnetlib import SB
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 # Realizar agrupamento k-means (uma técnica básica de mineração de dados)
 from sklearn.cluster import KMeans
 
@@ -29,36 +27,3 @@ netflix_limpo = netflix.dropna()
 print("\nNúmero de linhas antes da limpeza:", len(netflix))
 print("Número de linhas após a limpeza:", len(netflix_limpo))
 
-# Visualizar a distribuição das características usando histogramas
-netflix_limpo.hist()
-plt.show()
-
-# Verificar colunas com valores não numéricos
-for col in netflix_limpo.columns:
-    if netflix_limpo[col].dtype == 'object':
-        try:
-            pd.to_numeric(netflix_limpo[col])
-        except ValueError:
-            print(f"A coluna {col} contém valores não numéricos.")
-
-# Remover a coluna "Date" do DataFrame
-netflix_limpo_sem_data = netflix_limpo.drop(columns=['Date'])
-
-# Calcular e visualizar a correlação entre as características restantes
-plt.figure(figsize=(8, 6))
-sns.heatmap(netflix_limpo_sem_data.corr(), annot=True, cmap="coolwarm")
-plt.show()
-
-# # Selecionar apenas as colunas numéricas para o agrupamento
-# netflix_numerico = netflix_limpo_sem_data[['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]
-
-# # Aplicar o algoritmo KMeans para realizar o agrupamento
-# kmeans = KMeans(n_clusters=3, random_state=42)
-# kmeans.fit(netflix_numerico)
-
-# # Adicionar rótulos de cluster ao DataFrame
-# netflix_limpo_sem_data['Cluster'] = kmeans.labels_
-
-# # Exibir as primeiras 5 linhas do conjunto de dados com rótulos de cluster
-# print("\nPrimeiras 5 linhas do conjunto de dados com rótulos de cluster:")
-# print(netflix_limpo.head())
